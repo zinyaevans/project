4400fb8cb62b7124e47d356a752c38fd7111a570
nadya <example@gmail.com>
2023-03-24 11:34:39 +0500

    second

--- js/script.js
+++ js/script.js
@@ -17,4 +17,40 @@ answers[1] = prompt('Как ваша фамилия?', '');
 answers[2] = prompt('Сколько вам лет?', '');
 
 document.write(answers);
+console.log(typeof(answers));
+
+//Интерполяция, конкатенация
+
+const category = 'toys';
+console.log('https://someurl.com/' + category + '/' + '4');
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`Привет, ${user}`);
+
+// Операторы
+
+console.log(4 + +'5'); // унарный + перед строкой переводит строку в число
+
+let incr = 10,
+    decr = 10;
+
+// incr++; постфиксная форма - сначала фозвращает старое значение, а после учеличивает и уменьшает
+// decr--;
+
+console.log(++incr); // префиксная форма изменяет значение и возвращает новое
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8'); // === строгое равенство
+
+// && и
+// || или
+
+const isChecked = false,
+    isClose = true;
+
+console.log(isChecked || !isClose); // ! - оператор отрицания
 
